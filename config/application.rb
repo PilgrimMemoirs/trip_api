93141f0f7f2de0b9b194197cea5133d0e5898f83
CheezItMan <chris@adadevelopersacademy.org>
2018-11-14 15:32:11 -0800

    upgrade to rails 5.2 and use pg

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
@@ -16,23 +17,25 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Travel
+module TestApi
   class Application < Rails::Application
+  config.generators do |g|
+    # Force new test files to be generated in the minitest-spec style
+    g.test_framework :minitest, spec: true
+    # Always use .js files, never .coffee
+    g.javascript_engine :js
+  end
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :options]
-      end
-    end
-
     config.api_only = true
   end
 end
