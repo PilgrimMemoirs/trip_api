ddd9de25dab7346d4c5f3e0cc09519a497c51082
Dan Roberts <dan@adadevelopersacademy.org>
2019-05-28 16:10:36 -0700

    Destroy dependent reservations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,10 +1,10 @@
 class Trip < ApplicationRecord
-  CONTINENTS = ['Africa', 'Antartica', 'Asia', 'Australasia', 'Europe', 'North America', 'South America']
-  has_many :trip_reservations
+  CONTINENTS = ["Africa", "Antartica", "Asia", "Australasia", "Europe", "North America", "South America"]
+  has_many :trip_reservations, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
-  validates :continent, presence: true, inclusion: { in: CONTINENTS }
+  validates :continent, presence: true, inclusion: {in: CONTINENTS}
   validates :category, presence: true
-  validates :weeks, presence: true, numericality: { greater_than: 0 }
-  validates :cost, presence: true, numericality: { greater_than: 0 }
+  validates :weeks, presence: true, numericality: {greater_than: 0}
+  validates :cost, presence: true, numericality: {greater_than: 0}
 end
